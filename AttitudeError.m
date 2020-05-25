d151abbeeaeb914ae0a17ea79375dc6f5f4ea23f
Parth Viradiya <36721553+parthp08@users.noreply.github.com>
2020-05-25 18:54:15 -0400

    Update AttitudeError.m

--- AttitudeError.m
+++ AttitudeError.m
@@ -13,14 +13,6 @@ function [beta_BR,wB_BR] = AttitudeError(t, sigma_BN, wB_BN, RN, wN_RN)
 %   beta_BR: error quaternions of B-frame relative to R-frame
 %   w_BR: error angular vel of B-frame relative to R-frame
 
-%% pseudocode  -- remove this later
-% calculate BN = HN_t(t)
-% calculate beta_BN and wB_BN using DCM2QUAT and ??? (finite diff ??)
-% calcuate BR = BN*(RN')
-% calculate beta_BR from BR using DCM2QUAT
-% calculate wN_BR = (BN')*wB_BN - wN_RN
-% calculate wB_BR = BN*wN_BR
-
 BN = mrp2dcm(sigma_BN);
 
 RB = RN*(BN');
