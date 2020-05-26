5b321dd6b46ea22ae2805b531fc37ceeed7aaf53
Parth Viradiya <36721553+parthp08@users.noreply.github.com>
2020-05-25 22:54:45 -0400

    Update PosVelOrbit_test.m

--- PosVelOrbit_test.m
+++ PosVelOrbit_test.m
@@ -5,8 +5,8 @@ r_lmo = 3396.19+400; % for nano satellite
 gamma = 20;
 i = 30;
 theta_t0 = 60;
-%for t = 0:10:10000
-for t = 330
+for t = 0:10:10000
+%for t = 330
     [r,v] = PosVelOrbit(r_lmo, gamma, i, theta_t0, t);
     figure(1);
     plot(t,r(1),'.');
